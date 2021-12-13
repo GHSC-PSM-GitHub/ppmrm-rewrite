0a7956a1f9e529448fe3b8666b4b5d8f9a79a473
Eyassu Getachew <eyassug@gmail.com>
2021-12-13 18:01:37 +0300

    added BlobStoring module

--- src/PPMRm.Domain.Shared/PPMRmDomainSharedModule.cs
+++ src/PPMRm.Domain.Shared/PPMRmDomainSharedModule.cs
@@ -1,4 +1,4 @@
-﻿using PPMRm.Localization;
+using PPMRm.Localization;
 using Volo.Abp.AuditLogging;
 using Volo.Abp.BackgroundJobs;
 using Volo.Abp.FeatureManagement;
@@ -12,6 +12,7 @@ using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
 using Volo.Abp.Validation.Localization;
 using Volo.Abp.VirtualFileSystem;
+using Volo.Abp.BlobStoring.Database;
 
 namespace PPMRm
 {
@@ -25,6 +26,7 @@ namespace PPMRm
         typeof(AbpSettingManagementDomainSharedModule),
         typeof(AbpTenantManagementDomainSharedModule)
         )]
+    [DependsOn(typeof(BlobStoringDatabaseDomainSharedModule))]
     public class PPMRmDomainSharedModule : AbpModule
     {
         public override void PreConfigureServices(ServiceConfigurationContext context)
--- src/PPMRm.Domain/PPMRmDomainModule.cs
+++ src/PPMRm.Domain/PPMRmDomainModule.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using PPMRm.MultiTenancy;
 using Volo.Abp.AuditLogging;
@@ -13,6 +13,7 @@ using Volo.Abp.PermissionManagement.Identity;
 using Volo.Abp.PermissionManagement.IdentityServer;
 using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
+using Volo.Abp.BlobStoring.Database;
 
 namespace PPMRm
 {
@@ -29,6 +30,7 @@ namespace PPMRm
         typeof(AbpTenantManagementDomainModule),
         typeof(AbpEmailingModule)
     )]
+    [DependsOn(typeof(BlobStoringDatabaseDomainModule))]
     public class PPMRmDomainModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
