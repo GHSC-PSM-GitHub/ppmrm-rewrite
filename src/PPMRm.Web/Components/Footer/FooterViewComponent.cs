2f35549d509f724369ffdf21d03158b7f37e1cc1
Eyassu Getachew <eyassug@gmail.com>
2021-12-15 16:05:11 +0300

    added footer component view

---
+++ src/PPMRm.Web/Components/Footer/FooterViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace PPMRm.Web.Components.Footer
+{
+    public class FooterViewComponent : AbpViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("/Components/Footer/Default.cshtml");
+        }
+    }
+}
