2f6222ebfc8a9660b5a0860e433bc09074e5902e
Eyassu Getachew <eyassug@gmail.com>
2021-12-29 16:43:47 +0300

    added AggregateBase and AggregateRepository

---
+++ src/PPMRm.Domain/AggregateBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Volo.Abp.Domain.Entities;
+
+namespace PPMRm
+{
+    public abstract class AggregateBase : AggregateBase<string>
+    {
+        public override string StreamId => Id;
+    }
+
+    public abstract class AggregateBase<TKey> : Entity<TKey>, IAggregateBase
+    {
+        // For protecting the state, i.e. conflict prevention
+        // The setter is only public for setting up test conditions
+        public long Version { get; set; }
+
+        public virtual string StreamId => Id.ToString();
+
+        // JsonIgnore - for making sure that it won't be stored in inline projection
+        [JsonIgnore]
+        private readonly List<object> _uncommittedEvents = new List<object>();
+
+        // Get the deltas, i.e. events that make up the state, not yet persisted
+        public IEnumerable<object> GetUncommittedEvents()
+        {
+            return _uncommittedEvents;
+        }
+
+        // Mark the deltas as persisted.
+        public void ClearUncommittedEvents()
+        {
+            _uncommittedEvents.Clear();
+        }
+
+        protected void AddUncommittedEvent(object @event)
+        {
+            // add the event to the uncommitted list
+            _uncommittedEvents.Add(@event);
+        }
+    }
+}
---
+++ src/PPMRm.Marten/Repository/AggregateRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Marten;
+using Volo.Abp.Domain.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace PPMRm.Repository
+{
+    public sealed class AggregateRepository : DomainService
+    {
+        private readonly IDocumentStore store;
+
+        public AggregateRepository(IDocumentStore store)
+        {
+            this.store = store;
+        }
+
+        public async Task StoreAsync(AggregateBase aggregate)
+        {
+            using (var session = store.OpenSession())
+            {
+                // Take non-persisted events, push them to the event stream, indexed by the aggregate ID
+                var events = aggregate.GetUncommittedEvents().ToArray();
+                session.Events.Append(aggregate.Id, aggregate.Version, events);
+                await session.SaveChangesAsync();
+            }
+            aggregate.ClearUncommittedEvents();
+        }
+
+        public async Task<T> LoadAsync<T>(string id, int? version = null) where T : AggregateBase
+        {
+            using var session = store.LightweightSession();
+            var aggregate = await session.Events.AggregateStreamAsync<T>(id, version ?? 0);
+            return aggregate ?? throw new InvalidOperationException($"No aggregate by id {id}.");
+        }
+    }
+
+}
