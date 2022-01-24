32236440f484cb1d9db4d8e03d5b752f2e8d5042
Eyassu Getachew <eyassug@gmail.com>
2022-01-24 15:40:43 +0300

    added ARTMIS changeset constants

---
+++ src/PPMRm.Domain.Shared/ARTMIS/ChangeIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPMRm.ARTMIS
+{
+    public enum ChangeIndicator
+    {
+        Insert = 1,
+        Delete = 2,
+        Update = 3
+    }
+}
