9683f6a8c9655be0422b9a5f4720597573b170bf
Eyassu Getachew <eyassug@gmail.com>
2022-02-22 13:07:51 +0300

    added reference to PPMRm.Marten project

--- src/PPMRm.Marten/PPMRmStoreOptions.cs
+++ src/PPMRm.Marten/PPMRmStoreOptions.cs
@@ -1,6 +1,7 @@
 ﻿using Marten;
 using Marten.Events.Projections;
 using Microsoft.Extensions.Hosting;
+using PPMRm.Projections;
 using Weasel.Postgresql;
 
 namespace PPMRm
@@ -12,15 +13,22 @@ namespace PPMRm
             Connection(connectionString);
 
             // AutoCreate Schema
-            if(!environment.IsProduction())
+            if(environment != null && !environment.IsProduction())
                 AutoCreateSchemaObjects = AutoCreate.All;
 
             // Add Schema Registries
             Schema.Include<PPMRmMartenRegistry>();
             Schema.For<Items.Item>().Index(x => x.Name);
+            Schema.For<ARTMIS.OrderLines.OrderLine>().Duplicate(o => o.ProductId);
+            Schema.For<ARTMIS.OrderLines.OrderLine>().Duplicate(o => o.CountryId);
+            Schema.For<ARTMIS.OrderLines.OrderLine>().Duplicate(o => o.OrderNumber);
+            Schema.For<ARTMIS.OrderLines.OrderLine>().Duplicate(o => o.OrderLineNumber);
+            Schema.For<ARTMIS.OrderLines.OrderLine>().Index(o => new { o.OrderNumber, o.OrderLineNumber });
 
+            Projections.Add(new OrderLineProjection(), ProjectionLifecycle.Inline);
+            Events.StreamIdentity = Marten.Events.StreamIdentity.AsString;
             // Run the Order as an inline projection
-            Projections.SelfAggregate<ARTMIS.Orders.Order>(ProjectionLifecycle.Inline);
+            //Projections.SelfAggregate<ARTMIS.Orders.Order>(ProjectionLifecycle.Inline);
             // Add InitialData Seed
             // InitialData.Add(new InitialData(InitialDatasets.Products));
         }
