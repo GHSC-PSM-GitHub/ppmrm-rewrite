ee95b77b704ac4a88a377baf15d1bed650c69d65
Eyassu Getachew <eyassug@gmail.com>
2022-03-11 15:43:15 +0300

    added ProgramProduct razor page

---
+++ src/PPMRm.Web/Pages/PeriodReports/ProgramProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PPMRm.Web.Pages.PeriodReports
+{
+    public class ProgramProductModel : PageModel
+    {
+        public void OnGet(string periodReportId, int programId, string id)
+        {
+        }
+    }
+}
