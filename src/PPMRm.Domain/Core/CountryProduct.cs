97dba78ecde366e9b140de56eaafc85043575669
Eyassu Getachew <eyassug@gmail.com>
2022-03-16 20:45:04 +0300

    added countryproduct & country program associations

---
+++ src/PPMRm.Domain/Core/CountryProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+
+namespace PPMRm.Core
+{
+    public class CountryProduct : Entity
+    {
+        private CountryProduct()
+        {
+
+        }
+        internal CountryProduct(string countryId, string productId)
+        {
+            CountryId = Check.NotNullOrEmpty(countryId, nameof(countryId));
+            ProductId = Check.NotNullOrEmpty(productId, nameof(productId));
+        }
+        public string CountryId { get; private set; }
+        public string ProductId { get; private set; }
+
+        public override object[] GetKeys()
+        {
+            return new object[] { CountryId, ProductId };
+        }
+    }
+}
---
+++ src/PPMRm.Domain/Core/CountryProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+
+namespace PPMRm.Core
+{
+    public class CountryProgram : Entity
+    {
+        private CountryProgram()
+        {
+
+        }
+        internal CountryProgram(string countryId, int programId)
+        {
+            CountryId = Check.NotNullOrEmpty(countryId, nameof(countryId));
+            ProgramId = programId;
+        }
+        public string CountryId { get; private set; }
+        public int ProgramId { get; private set; }
+
+        public override object[] GetKeys()
+        {
+            return new object[] { CountryId, ProgramId };
+        }
+    }
+}
