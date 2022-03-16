b75c94418296c50c84a7e8f5acf0c7a09159714d
Eyassu Getachew <eyassug@gmail.com>
2022-03-16 19:22:14 +0300

    added Flags attribute to SOHLevel enum

--- src/PPMRm.Domain.Shared/PeriodReports/SOHLevel.cs
+++ src/PPMRm.Domain.Shared/PeriodReports/SOHLevel.cs
@@ -4,21 +4,22 @@ using System.Text;
 
 namespace PPMRm.PeriodReports
 {
+    [Flags]
     public enum SOHLevel
     {
         CentralWarehouse = 1,
         ZonalWarehouse = 2,
-        RegionalWarehouse=3,
-        ProvincialWarehouse=4,
-        StateWarehouse=5,
-        LGAWarehouse=6,
-        IntermediateWarehouse=7,
-        DistrictWarehouse=8,
-        CountyWarehouse=9,
-        MunicipalWarehouse=10,
-        SubCountryWarehouse=11,
-        SubDistrictWarehouse=12,
-        ServiceDeliveryPoints=13,
-        //JointMedicalStore=14
+        RegionalWarehouse= 4,
+        ProvincialWarehouse=8,
+        StateWarehouse=16,
+        LGAWarehouse=32,
+        IntermediateWarehouse=64,
+        DistrictWarehouse=128,
+        CountyWarehouse=256,
+        MunicipalWarehouse=512,
+        SubCountryWarehouse=1024,
+        SubDistrictWarehouse=2048,
+        ServiceDeliveryPoints=4096,
+        //JointMedicalStore=
     }
 }
