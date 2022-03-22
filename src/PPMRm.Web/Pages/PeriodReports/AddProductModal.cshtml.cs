3b85d13a0d9985f685281da13f6cbb9c3154ac87
Eyassu Getachew <eyassug@gmail.com>
2022-03-22 17:51:45 +0300

    fixed SOH Level display value pair on AddProductModal

--- src/PPMRm.Web/Pages/PeriodReports/AddProductModal.cshtml.cs
+++ src/PPMRm.Web/Pages/PeriodReports/AddProductModal.cshtml.cs
@@ -41,7 +41,7 @@ namespace PPMRm.Web.Pages.PeriodReports
         {
             Products = (await ProductRepository.ToListAsync()).OrderBy(p => p.Name).Select(p => new SelectListItem { Value = p.Id, Text = p.Name }).ToList();
             Programs = (await ProgramRepository.ToListAsync()).Select(p => new SelectListItem { Value = $"{p.Id}", Text = p.Name }).ToList();
-            SOHLevelOptions = Enum.GetValues<SOHLevel>().Select(l => new SelectListItem { Value = $"{(int)l}", Text = L[$"Enum:SOHLevel:{(int)l}"]}).ToList();
+            SOHLevelOptions = Enum.GetValues<SOHLevel>().Select(l => new SelectListItem { Value = $"{(int)l}", Text = L[$"Enum:SOHLevel:{l}"] }).ToList();
             ProgramName = Programs.SingleOrDefault(p => p.Value == $"{programId}")?.Text;
             Product = new CreateUpdateProgramProductViewModel
             {
