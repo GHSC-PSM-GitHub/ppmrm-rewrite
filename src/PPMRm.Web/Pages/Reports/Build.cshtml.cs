b225d0561887d78f10ce142dfa66648cd69b9838
Eyassu Getachew <eyassug@gmail.com>
2022-04-22 14:43:50 +0300

    implemented datatables export and period picker for Build your own report

--- src/PPMRm.Web/Pages/Reports/Build.cshtml.cs
+++ src/PPMRm.Web/Pages/Reports/Build.cshtml.cs
@@ -9,36 +9,81 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
+using PPMRm.Products;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PPMRm.Web.Pages.Reports
 {
+    [BindProperties]
     public class BuildModel : PPMRmPageModel
     {
         ICountryRepository CountryRepository { get; }
+        IRepository<Product, string> ProductRepository { get; }
+        IRepository<Core.Program, int> ProgramRepository { get; }
 
-        public BuildModel(ICountryRepository countryRepository)
+        public BuildModel(ICountryRepository countryRepository, IRepository<Product, string> productRepository, IRepository<Core.Program, int> programRepository)
         {
             CountryRepository = countryRepository;
+            ProductRepository = productRepository;
+            ProgramRepository = programRepository;
         }
         public async Task OnGetAsync()
         {
             var allCountries = await CountryRepository.GetUserCountriesAsync();
             Countries = allCountries.OrderBy(c => c.Name).Select(c => new SelectListItem { Value = c.Id, Text = c.Name }).ToList();
             SelectedCountries = Countries.Select(c => c.Value).ToList();
+            var allProducts = await ProductRepository.GetQueryableAsync();
+            Products = allProducts.OrderBy(p => p.Name).ToList().Select(p => new SelectListItem { Value = p.Id, Text = p.Name }).ToList();
+            SelectedProducts = Products.Select(p => p.Value).ToList();
+            var allPrograms = await ProgramRepository.GetQueryableAsync();
+            Programs = allPrograms.OrderBy(p => p.Name).ToList().Select(p => new SelectListItem { Value = $"{p.Id}", Text = p.Name }).ToList();
+            SelectedPrograms = Programs.Select(p => p.Value).ToList();
             Months = Enumerable.Range(1, 12).Select(i => new SelectListItem { Value = $"{i}", Text = DateTimeFormatInfo.CurrentInfo.GetMonthName(i) }).ToList();
             SelectedMonth = 3; // TODO: Get latest month from period repo
             Years = Enumerable.Range(2021, 2).Select(i => new SelectListItem { Value = $"{i}", Text = $"{i}" }).ToList();
             SelectedYear = 2022; // TODO: Get latest year from period repo
         }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var allCountries = await CountryRepository.GetUserCountriesAsync();
+            Countries = allCountries.OrderBy(c => c.Name).Select(c => new SelectListItem { Value = c.Id, Text = c.Name }).ToList();
+            //SelectedCountries = Countries.Select(c => c.Value).ToList();
+            var allProducts = await ProductRepository.GetQueryableAsync();
+            Products = allProducts.OrderBy(p => p.Name).ToList().Select(p => new SelectListItem { Value = p.Id, Text = p.Name }).ToList();
+            //SelectedProducts = Products.Select(p => p.Value).ToList();
+            var allPrograms = await ProgramRepository.GetQueryableAsync();
+            Programs = allPrograms.OrderBy(p => p.Name).ToList().Select(p => new SelectListItem { Value = $"{p.Id}", Text = p.Name }).ToList();
+            //SelectedPrograms = Programs.Select(p => p.Value).ToList();
+           // Months = Enumerable.Range(1, 12).Select(i => new SelectListItem { Value = $"{i}", Text = DateTimeFormatInfo.CurrentInfo.GetMonthName(i) }).ToList();
+
+            return NoContent();
+        }
         public List<SelectListItem> Countries { get; set; } = new();
+        public List<SelectListItem> Products { get; set; } = new();
+        public List<SelectListItem> Programs { get; set; } = new();
         public List<SelectListItem> Years { get; set; } = new();
         public List<SelectListItem> Months { get; set; } = new();
         [DisplayName("Countries")]
+        [Required]
         public List<string> SelectedCountries { get; set; } = new();
+        [Required]
+        [DisplayName("Products")]
+        public List<string> SelectedProducts { get; set; } = new();
+        [Required]
+        [DisplayName("Programs")]
+        public List<string> SelectedPrograms { get; set; }
         [DisplayName("Year")]
         public int SelectedYear { get; set; }
         [DisplayName("Period")]
         public int SelectedMonth { get; set; }
+        [Required]
+        public string StartPeriod { get; set; }
+        [Required]
+        public string EndPeriod { get; set; }
+
+        //public int StartPeriodId => Convert.ToInt32(StartPeriod.Value.ToString("yyyyMM"));
+        //public int EndPeriodId => Convert.ToInt32(EndPeriod.Value.ToString("yyyyMM"));
     }
 }
--- src/PPMRm.Web/Pages/Reports/Index.js
+++ src/PPMRm.Web/Pages/Reports/Index.js
@@ -2,6 +2,8 @@
 
     var l = abp.localization.getResource('PPMRm');
 
+    
+
     var inputAction = function (requestData, dataTableSettings) {
         return {
             countries: $("#SelectedCountries").val(),
@@ -22,5 +24,106 @@
         maxHeight: 250
 
     });
+
+    $("#SelectedProducts").multiselect({
+        selectAllValue: 'multiselect-all',
+        includeSelectAllOption: true,
+        enableFiltering: true,
+        enableCaseInsensitiveFiltering: true,
+        // inherits the class of the button from the original select
+        //inheritClass: true,
+        buttonClass: 'form-control',
+        buttonWidth: '100%',
+        maxHeight: 250
+
+    });
+
+    $("#SelectedPrograms").multiselect({
+        selectAllValue: 'multiselect-all',
+        includeSelectAllOption: true,
+        enableFiltering: true,
+        enableCaseInsensitiveFiltering: true,
+        // inherits the class of the button from the original select
+        //inheritClass: true,
+        buttonClass: 'form-control',
+        buttonWidth: '100%',
+        maxHeight: 250
+
+    });
     $("span.multiselect-native-select").addClass("form-control p-0 border-0 text-left");
+
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // $(".period-picker").datepicker({
+    //     //altField: $(this).id + "Id",
+    //     //altFormat: "yyyymm",
+    //     format: "MM yyyy",
+    //     startView: "months",
+    //     minViewMode: "months",
+    // });
+
+    $("#StartPeriod").datepicker({
+        format: "MM yyyy",
+        startView: "months",
+        minViewMode: "months",
+    });
+
+    $("#EndPeriod").datepicker({
+        format: "MM yyyy",
+        startView: "months",
+        minViewMode: "months",
+    });
+
+    $('#tblReport').DataTable({
+        dom: 'Brt',
+        buttons: [
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [ 0, ':visible' ]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            'colvis'
+        ]
+    });
 });
