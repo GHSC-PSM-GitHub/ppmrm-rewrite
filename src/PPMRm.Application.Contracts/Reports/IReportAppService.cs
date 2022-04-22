5b974c8dc73ae7e294c6ca0f6a63d65a0b3150dc
Eyassu Getachew <eyassug@gmail.com>
2022-04-22 14:41:19 +0300

    added Chart.js

---
+++ src/PPMRm.Application.Contracts/Reports/IReportAppService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PPMRm.Reports
+{
+    public interface IReportAppService
+    {
+        List<StockStatusDto> GetStockStatusAsync(GetStockStatusDto request);
+    }
+}
---
+++ src/PPMRm.Application.Contracts/Reports/StockStatusDto.cs
@@ -0,0 +1,37 @@
+﻿using PPMRm.PeriodReports;
+using System;
+using System.Collections.Generic;
+
+namespace PPMRm.Reports
+{
+    public class StockStatusDto
+    {
+        public Core.CountryDto Country { get; set; }
+        public Core.ProgramDto Program { get; set; }
+        public Products.ProductDto Product { get; set; }
+        public Core.PeriodDto Period { get; set; }
+        public List<string> SOHLevels { get; set; }
+        public decimal SOH { get; set; }
+        public decimal AMC { get; set; }
+        public decimal? MOS => AMC > 0 ? SOH / AMC : null;
+        public DateTime? DateOfSOH { get; set; }
+        public decimal MinStock { get; set; }
+        public decimal MaxStock { get; set; }
+        public string SourceOfConsumption { get; set; }
+        public string ActionRecommended { get; set; }
+        public DateTime? DateActionNeededBy { get; set; }
+        public List<ShipmentSummaryDto> Shipments { get; set; }
+    }
+
+    public class ShipmentSummaryDto
+    {
+        public Guid Id { get; set; }
+        public string Supplier { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal? AMC { get; set; }
+        public decimal? MOS => AMC != null && AMC.Value > 0 ? Quantity / AMC : null;
+        public DateTime? ShipmentDate { get; set; }
+        public ShipmentDateType ShipmentDateType { get; set; }
+        public ShipmentDataSource DataSource { get; set; }
+    }
+}
