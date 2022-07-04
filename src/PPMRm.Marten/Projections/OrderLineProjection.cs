cdb4a9d1a4a37a0276f3303382c4e798b2f09737
Eyassu Getachew <eyassug@gmail.com>
2022-07-04 19:20:50 +0300

    changed shipment date logic to use LEDD & RADD

--- src/PPMRm.Marten/Projections/OrderLineProjection.cs
+++ src/PPMRm.Marten/Projections/OrderLineProjection.cs
@@ -50,16 +50,16 @@ namespace PPMRm.Projections
 
             // Set PPMRM Shipment Display Date
             // Initial logic that checks LEDD and RaDD
-            // orderLine.ShipmentDate = @event.ActualDeliveryDate ?? @event.LatestEstimatedDeliveryDate ?? @event.RevisedAgreedDeliveryDate ?? @event.EstimatedDeliveryDate ?? @event.RequestedDeliveryDate;
-            //orderLine.ShipmentDateType = @event.ActualDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.ActualDeliveryDate :
-            //                    @event.LatestEstimatedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
-            //                    @event.RevisedAgreedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
-            //                    @event.EstimatedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
-            //                    @event.RequestedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.RequestedDeliveryDate : null;
-            orderLine.ShipmentDate = @event.ActualDeliveryDate ?? @event.EstimatedDeliveryDate ?? @event.RequestedDeliveryDate;
+            orderLine.ShipmentDate = @event.ActualDeliveryDate ?? @event.LatestEstimatedDeliveryDate ?? @event.RevisedAgreedDeliveryDate ?? @event.EstimatedDeliveryDate ?? @event.RequestedDeliveryDate;
             orderLine.ShipmentDateType = @event.ActualDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.ActualDeliveryDate :
+                                @event.LatestEstimatedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
+                                @event.RevisedAgreedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
                                 @event.EstimatedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
                                 @event.RequestedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.RequestedDeliveryDate : null;
+            //orderLine.ShipmentDate = @event.ActualDeliveryDate ?? @event.EstimatedDeliveryDate ?? @event.RequestedDeliveryDate;
+            //orderLine.ShipmentDateType = @event.ActualDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.ActualDeliveryDate :
+            //                    @event.EstimatedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.EstimatedDeliveryDate :
+            //                    @event.RequestedDeliveryDate != null ? ARTMISConsts.OrderDeliveryDateTypes.RequestedDeliveryDate : null;
         }
 
         public OrderLine Create(OrderLineInserted @event)
