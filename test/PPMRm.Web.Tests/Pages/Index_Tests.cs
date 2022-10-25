bbb9b9fbb1755b12efca7ec746abab66ae7ce4e5
Eyassu Getachew <eyassug@gmail.com>
2022-10-25 17:21:45 +0300

    updated dotnet version to 5.0.4xx

---
+++ test/PPMRm.Web.Tests/Pages/Index_Tests.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+using System;
+
+namespace PPMRm.Pages
+{
+    public class Index_Tests
+    {
+        [Fact]
+        public async Task Welcome_Page()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
