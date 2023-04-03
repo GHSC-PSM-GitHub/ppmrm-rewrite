2f4ed4b8a7d6f63d9dcdaaa3d013ad2c4d4f3084
Eyassu Getachew <eyassug@gmail.com>
2023-04-03 22:32:42 +0300

    updated connection strings

--- src/PPMRm.Domain/PeriodReports/PeriodReportSeedContributor.cs
+++ src/PPMRm.Domain/PeriodReports/PeriodReportSeedContributor.cs
@@ -37,7 +37,7 @@ namespace PPMRm.PeriodReports
         [UnitOfWork]
         public async Task SeedAsync(DataSeedContext context)
         {
-            var periods = new int[] { 202209 };
+            var periods = new int[] { 202303 };
             foreach (var pId in periods)
             {
                 if (await Repository.CountAsync(pr => pr.PeriodId == pId) == 0)
@@ -79,3 +79,4 @@ namespace PPMRm.PeriodReports
         }
     }
 }
+
